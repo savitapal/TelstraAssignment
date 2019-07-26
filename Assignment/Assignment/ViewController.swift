c17fbb28b54d9fcf91869a03b8d650885761b7e8
Savita <palsavita93@gmail.com>
2019-07-26 16:56:57 +0530

    Initial commit

---
+++ Assignment/Assignment/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by test on 26/07/19.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.view.backgroundColor = UIColor.white
+    }
+    
+    
+}
+
